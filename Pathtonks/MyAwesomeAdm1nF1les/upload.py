6596059c5f57dc5fb7e3a20f4769f327a9e4237e
Fabien Barre <fbarre96@hotmail.fr>
2021-12-07 06:12:31 -0800

    Added CSS

--- Pathtonks/MyAwesomeAdm1nF1les/upload.py
+++ Pathtonks/MyAwesomeAdm1nF1les/upload.py
@@ -2,6 +2,7 @@ from __main__ import app
 from flask import render_template, request
 import os
 
+# No one will be able to guess these routes and the secret
 @app.route("/AwesomeAdm1nR0ute/upload")
 def upload():
     return render_template('upload.html')
@@ -10,15 +11,19 @@ def upload():
 def upload_file():
      secret = request.form.get('secret', default=None, type = str)
      if secret is None:
-          return "Missing a secret.", 400
+          return render_template("400.html", e= "Missing a secret."), 400
      if secret != 'MYAWESOME_S3cret_Is_Secure!':
-          return "Invalid secret value.", 503
+          return render_template("400.html", e= "Invalid secret value."), 400
      dir_path = os.path.dirname(os.path.realpath(__file__))
      upload_dir = "../uploads"
      f = request.files['file']
      filename = f.filename.replace("..","_").strip()
      if filename == "":
-          return "Empty filename provided", 400
+          return render_template("400.html", e= "Empty filename provided"), 400
      full_path = os.path.join(dir_path, upload_dir, filename)
+     if dir_path not in full_path:
+          return render_template("400.html", e= "Restricted to "+dir_path), 400
+     if "main.py" in filename or "upload.py"  in filename:
+          return render_template("400.html", e= "I dunno how but you should not be able to replace those."), 400
      f.save(full_path)
      return 'file uploaded successfully in '+full_path
--- Pathtonks/main.py
+++ Pathtonks/main.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 from MyAwesomeAdm1nF1les import upload
 
 
+
 def zip_create(path):
     # create a ZipFile object
     tempfile.gettempdir()
@@ -22,6 +23,8 @@ def zip_create(path):
             # Iterate over all the files in directory
             for folderName, subfolders, filenames in os.walk(path):
                 for filename in filenames:
+                    if "flag.txt" in filename:
+                        continue
                     #create complete filepath of file in directory
                     filePath = os.path.join(folderName, filename)
                     # Add file to zip
@@ -33,19 +36,30 @@ def zip_create(path):
 @app.route("/")
 def index():
     return render_template('index.html')
+@app.route("/tonks")
+def tonks():
+    return render_template('tonks.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
 @app.route("/download")
 def download():
     filepath = request.args.get('filepath', default=None, type = str)
     if filepath is None:
-        return "missing filepath arg.", 400
+        return render_template("400.html", e= "missing filepath arg."), 400
+    if filepath.startswith("C:\\"):
+        return render_template("400.html", e="What are you even trying darklord?"), 400
     # filepath must not end by 'y' nor '.' to prevent downloading main.py and not end with a / or \ to avoid directory download
     if not re.match(r"[a-zA-Z\./\\]+[^y\\/\s\.]$", filepath):
-        return "The given filepath does not end with a valid file name", 400
+        return render_template("400.html", e=  "The given filepath does not end with a valid file name"), 400
+    # This should prevent path traversal :
     filename = os.path.normpath(os.path.basename(filepath))
     if not os.path.exists(filename):
-        return "This file does not exist.", 400
-    # ZIP IT FOR BAD CONNECTIONS
+        return render_template("400.html", e=  "This file does not exist."), 400
+    # ADDED ZIP FOR BAD CONNECTIONS
     zip_path = zip_create(filename)
     return send_file(zip_path)
     
